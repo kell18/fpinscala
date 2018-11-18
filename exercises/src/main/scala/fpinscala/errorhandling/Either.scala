a1277ddd358751f9f023a9b248c51850b794224a
kell18 <bikeev.albert@clover.global>
2018-11-18 18:18:47 +0300

    Complete Errors handling with Either tasks (4)

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -4,21 +4,45 @@ package fpinscala.errorhandling
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = ???
+ def map[B](f: A => B): Either[E, B]
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+ def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B]
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+ def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B]
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+ def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C]
 }
-case class Left[+E](get: E) extends Either[E,Nothing]
-case class Right[+A](get: A) extends Either[Nothing,A]
+
+case class Left[+E](get: E) extends Either[E,Nothing] {
+  override def map[B](f: Nothing => B): Either[E, B] = this
+
+  override def flatMap[EE >: E, B](f: Nothing => Either[EE, B]): Either[EE, B] = this
+
+  override def orElse[EE >: E, B >: Nothing](b: => Either[EE, B]): Either[EE, B] = b
+
+  override def map2[EE >: E, B, C](b: Either[EE, B])(f: (Nothing, B) => C): Either[EE, C] = this
+}
+
+case class Right[+A](get: A) extends Either[Nothing,A] {
+  override def map[B](f: A => B) = Right(f(get))
+
+  override def flatMap[EE >: Nothing, B](f: A => Either[EE, B]) = f(get)
+
+  override def orElse[EE >: Nothing, B >: A](b: => Either[EE, B]) = this
+
+  override def map2[EE >: Nothing, B, C](b: Either[EE, B])(f: (A, B) => C) = b map { 
+    bInner => f(get, bInner)
+  }
+}
 
 object Either {
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = {
+    es.foldRight[Either[E, List[B]]](Right(List.empty)) {
+      case (i, acc) => f(i).map2(acc) { _ :: _ } 
+    }
+  }
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = ???
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = traverse(es)(identity)
 
   def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
     if (xs.isEmpty) 
